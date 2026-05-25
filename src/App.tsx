bdd95bb16797d68a11b672c0907c032b94d8c122
Edwin Moreno <edwinmoreno903@gmail.com>
2026-05-24 20:12:32 -0500

    Componente Propinas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import MenuItems from "./components/MenuItem";
 import OrderContents from "./components/OrderContents";
 import OrderTotals from "./components/OrderTotals";
+import TipPercentageForm from "./components/TipPercentageForm";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
 function App() {
-  const { order, addItem, removeItem } = useOrder();
+  const { order, tip, setTip, addItem, removeItem, saveOrder } = useOrder();
 
   return (
     <>
@@ -26,9 +27,17 @@ function App() {
         </div>
 
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <OrderContents order={order} removeItem={removeItem} />
+          {order.length ? (
+            <>
+              <OrderContents order={order} removeItem={removeItem} />
 
-          <OrderTotals order={order} />
+              <TipPercentageForm setTip={setTip} />
+
+              <OrderTotals order={order} tip={tip} saveOrder={saveOrder} />
+            </>
+          ) : (
+          <p className="text-center">La orden esta vacia</p>
+          )}
         </div>
       </main>
     </>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -4,13 +4,19 @@ import { formatCurrency } from "../helpers";
 
 type OrderTotalsProps = {
   order: OrderItem[];
+  tip: number;
+  saveOrder: () => void
 };
 
-export default function OrderTotals({ order }: OrderTotalsProps) {
+export default function OrderTotals({ order, tip, saveOrder }: OrderTotalsProps) {
   const subtotalAmount = useMemo(
     () => order.reduce((total, item) => total + item.quantity * item.price, 0),
     [order],
   );
+
+  const tipAmount = useMemo(() => subtotalAmount * tip, [tip, subtotalAmount]);
+
+  const totalAmount = subtotalAmount + tipAmount;
 
   return (
     <>
@@ -23,14 +29,22 @@ export default function OrderTotals({ order }: OrderTotalsProps) {
 
         <p>
           Propinas: {""}
-          <span className="font-bold">$0</span>
+          <span className="font-bold">{formatCurrency(tipAmount)}</span>
         </p>
 
         <p>
           Total a pagar: {""}
-          <span className="font-bold">$0</span>
+          <span className="font-bold">{formatCurrency(totalAmount)}</span>
         </p>
       </div>
+
+      <button
+        className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10 disabled:cursor-not-allowed cursor-pointer"
+        disabled={totalAmount === 0}
+        onClick={saveOrder}
+      >
+        Guardar Orden
+      </button>
     </>
   );
 }
